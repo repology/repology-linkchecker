9b26b3e644cb7702c223652d016db829258f1b56
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-03-30 03:52:00 +0300

    Create test database for CI

---
+++ ci/create_database.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE repology;
+CREATE USER repology WITH PASSWORD 'repology';
+GRANT ALL PRIVILEGES ON DATABASE repology_test TO repology_test;
+
+\c repology repology
+
+CREATE TABLE links (
+    url text NOT NULL PRIMARY KEY,
+    last_checked timestamp with time zone,
+
+	ipv4_last_success timestamp with time zone,
+	ipv4_last_failure timestamp with time zone,
+	ipv4_success boolean,
+	ipv4_status_code smallint,
+	ipv4_permanent_redirect_target text,
+
+	ipv6_last_success timestamp with time zone,
+	ipv6_last_failure timestamp with time zone,
+	ipv6_success boolean,
+	ipv6_status_code smallint,
+	ipv6_permanent_redirect_target text
+);
+
+INSERT INTO links(url) VALUES
+	('http://repology.org/'),
+	('https://repology.org/'),
+	('https://repology.org/nonexistent'),
+	('https://nonexistent.repology.org/');
